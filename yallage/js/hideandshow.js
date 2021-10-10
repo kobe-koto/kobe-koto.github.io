bd315f911bdf05cf456df334b5804affb03886b9
evenstar3905 <kobe_koto@outlook.com>
2021-10-10 17:05:08 +0800

    一次普通的commit（确信

---
+++ yallage/js/hideandshow.js
@@ -0,0 +1,7 @@
+﻿// Encoding: UTF-8
+function show(ElementID) {
+	document.getElementById(ElementID).style.display = "block";
+}
+function hide(ElementID) {
+	document.getElementById(ElementID).style.display = "none";
+}
