bd81979a1b13afc0f7013991b3c18be3ca5fbed6
evenstar3905 <kobe_koto@outlook.com>
2021-10-30 17:37:25 +0800

    update?

--- yallage/js/hideandshow.js
+++ yallage/js/hideandshow.js
@@ -1,6 +1,8 @@
 ﻿// Encoding: UTF-8
+// Made with heart by evenstar3905 in AGPL-3.0 License License
+// copyright 2021 evenstar3905
 function show(ElementID) {
-	document.getElementById(ElementID).style.display = "block";
+	document.getElementById(ElementID).style.display = "unset";
 }
 function hide(ElementID) {
 	document.getElementById(ElementID).style.display = "none";
