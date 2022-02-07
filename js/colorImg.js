97451e2c6b47f6b239d1fc4a992e71ca47dc79fb
kobe koto <kobe_koto@outlook.com>
2022-02-07 18:26:03 +0800

    dagj

--- js/colorImg.js
+++ js/colorImg.js
@@ -7,18 +7,21 @@ function random (intmin,intmax) {
 	}
 }
 
-var requestURL = "../assets/ColorImg.txt";
-var request = new XMLHttpRequest();
-request.withCredentials = true;
-request.crossOrigin = "Anonymous";
-request.open("GET", requestURL,true);
-request.send(null);
-request.onload = function () {
-	ColorImgJson = request.response;
-	console.log("done!")
-}
-
 function windowload() {
+	document.getElementById("NextBtn").style.display = "none";
+	var requestURL = "../assets/ColorImg.txt";
+	var request = new XMLHttpRequest();
+	request.open("GET", requestURL,true);
+	request.send(null);
+	request.onload = function () {
+		ColorImgJson = JSON.parse(request.response);
+		console.log("loadListDone!")
+		document.getElementById("NextBtn").style.display = "unset";
+
+		loadNextPic();
+	}
+}
+function loadNextPic() {
 
 	// hide o-img
 	document.getElementById("downloadColorPic").style.display = "none";
@@ -30,17 +33,20 @@ function windowload() {
 	// loading
 	var picNum = random("1","866");
 	// var picLink = "https://drive-koto.vercel.app/api?path=/Image/colorImg/" + picNum + ".png&raw=true";
-	var picLink = ColorImgJson.folder.value[picNum]["@microsoft.graph.downloadUrl"];
+	// var picLink = ColorImgJson.folder.value[picNum]["@microsoft.graph.downloadUrl"];
+	var picLink = "https://drive-koto.vercel.app/api?path=/Image/GetColorImg/" + ColorImgJson.pics[picNum].name + "&raw=true";
+
 
 	document.getElementById("downloadColorPic").href = picLink;
-	document.getElementById("downloadColorPic").download = picNum + ".png";
+	document.getElementById("downloadColorPic").download = ColorImgJson.pics[picNum].name;
 	document.getElementById("colorPic").src = picLink;
 
 	// load done
 	document.getElementById("colorPic").onload = function () {
 		document.getElementById("loader").style.display = "none";
-		document.getElementById("picNum").innerHTML = "PicNum = " + picNum;
+		document.getElementById("picNum").innerHTML = "Pic = " + ColorImgJson.pics[picNum].name;
 		document.getElementById("downloadColorPic").style.display = "unset";
+		console.log("Image load successfully.")
 	}
 
 	// load error
