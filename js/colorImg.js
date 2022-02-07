184fababc2fc70327656763b306d80edad26c2f0
kobe koto <kobe_koto@outlook.com>
2022-02-07 17:35:54 +0800

    afsdgsg

--- js/colorImg.js
+++ js/colorImg.js
@@ -1,44 +1,61 @@
 function random (intmin,intmax) {
-    while (!isNaN(intmax) && !isNaN(intmin) && !intmax.match(/(-)/i)) {
-        var randomno = Math.round(intmax * Math.random());
-        if (randomno <= intmax && randomno >= intmin) {
-            return randomno;
-        }
-    }
+	while (!isNaN(intmax) && !isNaN(intmin) && !intmax.match(/(-)/i)) {
+		var randomno = Math.round(intmax * Math.random());
+		if (randomno <= intmax && randomno >= intmin) {
+			return randomno;
+		}
+	}
 }
 
+var requestURL = "../assets/ColorImg.txt";
+var request = new XMLHttpRequest();
+request.withCredentials = true;
+request.crossOrigin = "Anonymous";
+request.open("GET", requestURL,true);
+request.send(null);
+request.onload = function () {
+	ColorImgJson = request.response;
+	console.log("done!")
+}
 
 function windowload() {
 
-    // hide o-img
-    document.getElementById("downloadColorPic").style.display = "none";
-    document.getElementById("loader").style.display = "unset";
-    document.getElementById("picNum").innerHTML = "loading";
-    console.log("loading");
+	// hide o-img
+	document.getElementById("downloadColorPic").style.display = "none";
+	document.getElementById("loader").style.display = "unset";
 
-    // loading
-    var picNum = random("1","220");
-    var picLink = "https://drive-koto.vercel.app/api?path=/Image/colorImg/" + picNum + ".png&raw=true";
+	document.getElementById("picNum").innerHTML = "loading";
+	console.log("loading");
 
-    document.getElementById("downloadColorPic").href = picLink;
-    document.getElementById("downloadColorPic").download = picNum + ".png";
-    document.getElementById("colorPic").src = picLink;
+	// loading
+	var picNum = random("1","866");
+	// var picLink = "https://drive-koto.vercel.app/api?path=/Image/colorImg/" + picNum + ".png&raw=true";
+	var picLink = ColorImgJson.folder.value[picNum]["@microsoft.graph.downloadUrl"];
 
-    // load done
-    document.getElementById("colorPic").onload = (
-        function () {
-            document.getElementById("picNum").innerHTML = "PicNum = " + picNum;
-            document.getElementById("downloadColorPic").style.display = "unset";
-            document.getElementById("loader").style.display = "none";
-        }
-    )
-    // load error
-    document.getElementById("colorPic").onerror = (
-        function () {
-            document.getElementById("loader").style.display = "none";
-            document.getElementById("picNum").innerHTML = "Pic cannot load.Check you network connect.";
-            console.error("Pic cannot load.Check you network connect.")
-        }
-    )
+	document.getElementById("downloadColorPic").href = picLink;
+	document.getElementById("downloadColorPic").download = picNum + ".png";
+	document.getElementById("colorPic").src = picLink;
+
+	// load done
+	document.getElementById("colorPic").onload = function () {
+		document.getElementById("loader").style.display = "none";
+		document.getElementById("picNum").innerHTML = "PicNum = " + picNum;
+		document.getElementById("downloadColorPic").style.display = "unset";
+	}
+
+	// load error
+	document.getElementById("colorPic").onerror = function () {
+		document.getElementById("loader").style.display = "none";
+		document.getElementById("picNum").innerHTML = "Pic cannot load.Check you network connect.";
+		console.error("Pic cannot load.Check you network connect.")
+	}
 
 }
+
+
+
+
+
+
+
+
